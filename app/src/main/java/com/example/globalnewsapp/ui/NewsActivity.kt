fd4d67432728af5cce5a5e8c76629c009c4a9e26
Peter-cloud-web <petergichia165@gmail.com>
2021-11-11 21:40:04 +0300

    Add menu items with the necessary links in the nav drawer

--- app/src/main/java/com/example/globalnewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/example/globalnewsapp/ui/NewsActivity.kt
@@ -1,7 +1,12 @@
 package com.example.globalnewsapp.ui
 
+import android.content.Intent
+import android.content.Intent.ACTION_VIEW
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -18,6 +23,7 @@ import kotlinx.android.synthetic.main.activity_news.*
 class NewsActivity : AppCompatActivity() {
 
     lateinit var viewModels:NewsViewModel
+    lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +35,41 @@ class NewsActivity : AppCompatActivity() {
 
         setUpNavigation()
 
+        toggle = ActionBarDrawerToggle(this,drawer_layout,R.string.open,R.string.close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        navMenu.itemIconTintList = null
+
+        navMenu.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.github_repo ->
+                    startActivity(
+                        Intent(
+                         ACTION_VIEW,
+                         Uri.parse("https://github.com/Peter-cloud-web/GlobalNewsApp")
+                        )
+                    )
+                R.id.developer ->
+                    startActivity(
+                        Intent(
+                            ACTION_VIEW,
+                            Uri.parse("https://t.co/Y6b7mMOtRr?amp=1")
+                        )
+                    )
+            }
+            true
+        }
+
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun setUpNavigation(){
