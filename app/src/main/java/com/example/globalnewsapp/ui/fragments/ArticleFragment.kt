41cb4c8a530d7a99cf5967d9b99994dddf53f80e
Peter-cloud-web <petergichia165@gmail.com>
2021-11-11 14:59:41 +0300

    Add progress bar to the articles fragment

--- app/src/main/java/com/example/globalnewsapp/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/example/globalnewsapp/ui/fragments/ArticleFragment.kt
@@ -1,7 +1,9 @@
 package com.example.globalnewsapp.ui.fragments
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
@@ -23,6 +25,19 @@ class ArticleFragment: Fragment(R.layout.fragment_article) {
         webView.apply{
             webViewClient = WebViewClient()
             loadUrl(article.url)
+
+            webViewClient = object : WebViewClient(){
+                override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                    super.onPageStarted(view, url, favicon)
+                    progressBar.visibility = View.VISIBLE
+                }
+
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    progressBar.visibility = View.GONE
+                }
+
+            }
         }
         fab.setOnClickListener{
             viewModels.saveArticle(article)
