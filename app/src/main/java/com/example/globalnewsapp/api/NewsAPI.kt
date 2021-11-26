f6db0a9650b62e13b43c6bd9e1b94e8aec8cf5fb
Peter-cloud-web <petergichia165@gmail.com>
2021-11-26 06:55:11 +0300

    Enhance api authentication security

--- app/src/main/java/com/example/globalnewsapp/api/NewsAPI.kt
+++ app/src/main/java/com/example/globalnewsapp/api/NewsAPI.kt
@@ -1,24 +1,33 @@
 package com.example.globalnewsapp.api
 
+
+
+import com.example.globalnewsapp.BuildConfig
 import com.example.globalnewsapp.models.NewsResponse
 import com.example.globalnewsapp.util.Constants.Companion.API_KEY
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+
 interface NewsAPI {
+
+    companion object{
+         val api_key = BuildConfig.NEWS_API_KEY
+    }
+
 
     @GET("v2/top-headlines")
     suspend fun getBreakingNews(
         @Query("country") countryCode:String = "us",
         @Query("page") pageNumber:Int = 1,
-        @Query("apiKey") apiKey:String = API_KEY
+        @Query("apiKey") apiKey:String = NewsAPI.api_key
     ): Response<NewsResponse>
 
     @GET("v2/everything")
     suspend fun searchNews(
         @Query("q") searchQuery: String,
         @Query("page") pageNumber: Int = 1,
-        @Query("apiKey") apiKey: String = API_KEY
+        @Query("apiKey") apiKey: String = NewsAPI.api_key
     ):Response<NewsResponse>
 }
--- app/src/main/java/com/example/globalnewsapp/util/Constants.kt
+++ app/src/main/java/com/example/globalnewsapp/util/Constants.kt
@@ -3,11 +3,8 @@ package com.example.globalnewsapp.util
 class Constants {
 
     companion object {
-
-        const val API_KEY = "3b2efb43d09e45eb8c3258025a5e42ad"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
         const val QUERY_PAGE_SIZE= 20
-
     }
 }
